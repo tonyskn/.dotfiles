cc80c5a1a2de98bc938c4a0bba4035a613fd3e56
tonyskn <tonyskn@gmail.com>
2013-07-31 08:55:58 +0200

    Fix wlan interface name in xmobar config

--- _xmonad/xmobar/xmobarrc-monitors.hs
+++ _xmonad/xmobar/xmobarrc-monitors.hs
@@ -4,9 +4,9 @@ Config {
    , position = Bottom
    , commands =[ Run CpuFreq ["-t", "Freq:<cpu0>|<cpu1>|<cpu2>|<cpu3>", "-L", "0", "-H", "2", "-l", "green", "-n","lightblue", "-h", "red"] 50
                , Run Uptime [] 10
-               , Run Wireless "wlan0" [] 10
+               , Run Wireless "wlp3s0" [] 10
                , Run TopProc [] 10
                , Run CommandReader "~/.xmonad/xmobar/xmobarrc-monitors.sh" "extra"
-               , Run Network "wlan0" ["-L","0","-H","70","-l", "green", "-n","lightblue","-h","red"] 10
+               , Run Network "wlp3s0" ["-L","0","-H","70","-l", "green", "-n","lightblue","-h","red"] 10
                , Run BatteryP ["BAT0"] ["-t", "<timeleft>", "-c", "charge_full"] 10 ]
-   , template = "%wlan0wi%-%wlan0% ~ %top% ~ %cpufreq%}{%extra%|%battery% ~ %uptime% " }
+   , template = "%wlp3s0wi%-%wlp3s0% ~ %top% ~ %cpufreq%}{%extra%|%battery% ~ %uptime% " }
