fc72a1943fa854868ed1d8e29000164ff8128151
tonyskn <tonyskn@gmail.com>
2013-09-03 11:36:27 +0200

    Set weather indicator to Berlin in xmobar

--- _xmonad/xmobar/xmobarrc.hs
+++ _xmonad/xmobar/xmobarrc.hs
@@ -2,11 +2,11 @@
        bgColor = "black"
      , fgColor = "grey"
      , position = Top
-     , commands = [ Run Weather "LFPO" ["-t", "<tempC>°"] 50
+     , commands = [ Run Weather "EDDT" ["-t", "<tempC>°"] 50
                  , Run Cpu ["-L", "3", "-H", "50", "-l", "green", "-n", "lightblue", "-h", "red"] 10
                  , Run Memory ["-t", "Mem: <usedratio>%", "-L", "20", "-H", "50", "-l", "green", "-n", "lightblue", "-h", "red"] 10
                  , Run Uptime [] 10
                  , Run Date "%a %b %_d %H:%M" "date" 10
                  , Run StdinReader ]
-     , template = " %StdinReader%}{ %cpu% ~ %memory% ~ %uptime% ~ <fc=#ee9a00>%date% %LFPO%</fc> " }
+     , template = " %StdinReader%}{ %cpu% ~ %memory% ~ %uptime% ~ <fc=#ee9a00>%date% %EDDT%</fc> " }
 
