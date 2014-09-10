f5d8e6e5602ff0ea80b035e9876744826ffaf127
tonyskn <tonyskn@gmail.com>
2014-09-10 08:56:02 +0200

    [xmobar] native wifi finally working

--- _xmonad/xmobar/xmobarrc-monitors.hs
+++ _xmonad/xmobar/xmobarrc-monitors.hs
@@ -5,8 +5,8 @@ Config {
    , commands =[ Run Uptime [] 10
                , Run TopProc [] 10
                , Run CommandReader "~/.xmonad/xmobar/xmobarrc-monitors.sh" "extra"
-               , Run Network "enp0s20u3" ["-L","0","-H","70","-l", "green", "-n","lightblue","-h","red"] 10
+               , Run Network "wlp3s0" ["-L","0","-H","70","-l", "green", "-n","lightblue","-h","red"] 10
                , Run BatteryP ["BAT0", "BAT1"] ["-t", "Bat: <watts>|<left>%|<timeleft>"] 10
                , Run BatteryN ["BAT0"] ["-t", "Bat0: <watts>|<left>%"] 10 "battery0"
                , Run BatteryN ["BAT1"] ["-t", "Bat1: <watts>|<left>%"] 10 "battery1" ]
-   , template = "%enp0s20u3% ~ %top% ~ %extra%}{%battery0% ~ %battery1% ~ %battery% ~ %uptime% " }
+   , template = "%wlp3s0% ~ %top% ~ %extra%}{%battery0% ~ %battery1% ~ %battery% ~ %uptime% " }
