75a699ee8b2ef7b8a009e2e250b980846bc5003e
tonyskn <tonyskn@gmail.com>
2026-02-28 09:56:12 +0100

    switch to nvim's init.lua*

---
+++ _config/nvim/init.lua
@@ -0,0 +1,335 @@
+-- Leader must be set before plugins
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+-- General settings {{{
+
+vim.opt.laststatus = 2
+vim.opt.backupdir = vim.fn.expand("~/.vim/backup")
+vim.opt.directory = vim.fn.expand("~/.vim/backup")
+vim.opt.clipboard = "unnamed"
+vim.opt.scrolloff = 3
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.expand("~/.vim/vimundo-nvim")
+vim.opt.errorbells = false
+vim.opt.visualbell = true
+vim.opt.showcmd = true
+vim.opt.number = true
+vim.opt.ruler = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.autoindent = true
+vim.opt.listchars = { tab = "» ", eol = "¬", extends = "❯", precedes = "❮" }
+vim.opt.wildmode = { "list:longest", "list:full" }
+vim.opt.wildignore:append("*.o,*.obj,.git,*.rbc,*.class,.svn,vendor/gems/*")
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.colorcolumn = "120"
+vim.opt.shortmess:append("A")
+vim.opt.autoread = true
+vim.opt.breakindent = true
+vim.opt.shada = "'1024"
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.completeopt = { "longest", "menuone", "preview" }
+vim.opt.diffopt:append("iwhite")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "vim",
+  callback = function() vim.opt_local.foldmethod = "marker" end,
+})
+
+-- }}}
+
+-- Keybindings {{{
+
+local map = vim.keymap.set
+
+-- leader ; for original , behavior
+map("n", "<Leader>;", ",")
+-- toggle listchars
+map("n", "<Leader>s", "<cmd>set list!<CR>", { silent = true })
+-- clear search highlight
+map("n", "<Leader><space>", "<cmd>noh<CR>", { silent = true })
+-- don't move cursor on *
+map("n", "*", "*<C-o>")
+-- search for visual selection
+map("v", "*", "y/<C-r>\"<CR><C-o>")
+-- disable K manual
+map("n", "K", "<nop>")
+-- reselect visual block after indent
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+-- paste: auto-adjust indent and jump to end
+map("n", "p", "]p`]")
+map("v", "p", "]p`]")
+map("v", "y", "y`]")
+map("n", "P", "]P")
+-- navigate to next diff after action
+map("n", "do", "do]c", { remap = true })
+map("n", "dp", "dp]c", { remap = true })
+-- make Y behave like other capitals
+map("", "Y", "y$")
+-- improve up/down on wrapped lines
+map("n", "j", "gj")
+map("n", "k", "gk")
+-- keep search matches centered
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+-- exit terminal mode
+map("t", "<Esc>", "<C-\\><C-n>")
+
+-- emacs home/end in insert/command mode
+map("i", "<C-a>", "<Esc>I")
+map("i", "<C-e>", "<Esc>A")
+map("c", "<C-a>", "<Home>")
+map("c", "<C-e>", "<End>")
+-- space toggles folding
+map("n", "<Space>", "za")
+map("v", "<Space>", "za")
+-- quick fold navigation
+map("n", "zJ", "zjzmza")
+map("n", "zK", "zkzmza[z")
+-- window shortcuts
+map("n", "=", "<cmd>res<CR><cmd>vertical res<CR>", { silent = true })
+map("n", "<Leader>=", "<C-w>=")
+map("n", "<Leader>l", "<C-w>L")
+map("n", "<Leader>p", "<C-w>J")
+-- diff navigation
+map("", "<Leader>d", "]czz", { silent = true })
+map("", "<Leader>D", "[czz", { silent = true })
+-- close buffer and go up
+map("", "<Leader>q", "<cmd>x<CR><C-w>j", { silent = true })
+-- walk through files and diff against master
+map("n", "<Leader>id", "<C-w>f:resize<CR><C-j><C-j>:Gvdiff master<CR>", { silent = true })
+map("n", "<Leader>if", ":bd<CR>:bd<CR>j<Leader>id", { silent = true })
+map("n", "<Leader>iF", ":bd<CR>:bd<CR>dd<Leader>id", { silent = true })
+-- sudo write
+map("c", "w!!", "w !sudo tee % > /dev/null <CR>")
+-- replace visual selection with output of command
+map("v", "<Leader>!", function()
+  local cmd = vim.fn.input(":")
+  if cmd == "" then return end
+  local output = vim.fn.system("zsh -lc '" .. cmd .. "'"):gsub("\n$", "")
+  vim.fn.setreg("z", output)
+  vim.cmd('normal! gv"_d')
+  vim.cmd('normal! "zP')
+end)
+-- html escape visual selection
+map("v", "<Leader>x", ":!recode utf8..html<CR>")
+-- format text into columns
+map("n", "<Leader>t", ":%!column -t<CR>", { silent = true })
+-- diff registers a and b
+map("n", "<Leader>V", "<cmd>tabnew<CR><cmd>put a<CR><cmd>diffthis<CR><cmd>vnew<CR><cmd>put b<CR><cmd>diffthis<CR>", { silent = true })
+map("n", "<Leader>Q", "<cmd>windo bd!<CR>tabclose<CR>", { silent = true })
+-- comment with ,c (uses built-in gc)
+map("", ",c", "gc", { remap = true })
+-- plugin status
+map("n", "<Leader>S", "<cmd>Lazy<CR>", { silent = true })
+
+-- }}}
+
+-- Plugin settings (must be set before lazy.setup) {{{
+
+-- targets.vim
+vim.g.targets_pairs = "()b {}c []B"
+vim.g.targets_argOpening = "[({[]"
+vim.g.targets_argClosing = "[]})]]"
+
+-- fugitive
+vim.g.github_enterprise_urls = { "https://ghe.spotify.net" }
+
+-- REST console
+vim.g.vrc_set_default_mapping = 0
+vim.g.vrc_allow_get_request_body = 1
+vim.g.vrc_elasticsearch_support = 1
+vim.b.vrc_response_default_content_type = "application/json"
+vim.g.vrc_output_buffer_name = "__REST.json"
+vim.g.vrc_curl_opts = {
+  ["-b"] = vim.fn.expand("$HOME") .. "/.vim/backup/vrc_cookie_jar",
+  ["-c"] = vim.fn.expand("$HOME") .. "/.vim/backup/vrc_cookie_jar",
+  ["-s"] = "",
+  ["-L"] = "",
+  ["-k"] = "",
+}
+
+-- }}}
+
+-- Bootstrap lazy.nvim {{{
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- }}}
+
+-- Plugins {{{
+
+require("lazy").setup({
+
+  -- Colorscheme
+  {
+    "gbprod/nord.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme nord")
+    end,
+  },
+
+  -- Statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = {
+        theme = "nord",
+        icons_enabled = true,
+      },
+    },
+  },
+
+  -- Treesitter (parsers install via :TSInstall)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+  },
+
+  -- File tree (neo-tree, same keybindings as NERDTree)
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      close_if_last_window = true,
+      popup_border_style = "single",
+      window = { width = 60 },
+      filesystem = {
+        follow_current_file = { enabled = true, leave_dirs_open = true },
+        filtered_items = { hide_dotfiles = false },
+        bind_to_cwd = false,
+        cwd_target = { sidebar = "none", current = "none" },
+        use_libuv_file_watcher = true,
+      },
+      default_component_configs = {
+        indent = {
+          expander_collapsed = "+",
+          expander_expanded = "-",
+        },
+      },
+    },
+    keys = {
+      { "<Leader>nd", "<cmd>Neotree toggle<CR>", desc = "Toggle file tree" },
+      {
+        "<Leader>nf",
+        function()
+          local manager = require("neo-tree.sources.manager")
+          local state = manager.get_state("filesystem")
+          local current_root = state and state.path
+          local file_path = vim.fn.expand("%:p")
+
+          if current_root and file_path:find(current_root, 1, true) == 1 then
+            vim.cmd("Neotree reveal")
+          else
+            vim.cmd("Neotree reveal dir=" .. vim.fn.fnameescape(vim.fn.expand("%:p:h")))
+          end
+        end,
+        desc = "Reveal file in tree",
+      },
+    },
+  },
+
+  -- Telescope (fuzzy finder, ivy theme = bottom panel like fzf)
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    cmd = "Telescope",
+    keys = {
+      { "<Leader>nt", function() require("telescope.builtin").find_files({ hidden = true }) end, desc = "Find files" },
+      { "<Leader>nb", function() require("telescope.builtin").buffers() end, desc = "Buffers" },
+      { "<Leader>ne", function() require("telescope.builtin").oldfiles() end, desc = "Recent files" },
+      { "<Leader>nr", function() require("telescope.builtin").command_history() end, desc = "Command history" },
+      { "<Leader>nc", function() require("telescope.builtin").git_bcommits() end, desc = "Buffer commits" },
+      { "<Leader>ng", function() require("telescope.builtin").live_grep() end, desc = "Live grep" },
+      { "<Leader>G", function() require("telescope.builtin").grep_string() end, desc = "Grep word under cursor" },
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      telescope.setup({
+        defaults = require("telescope.themes").get_ivy({
+          layout_config = { height = 0.3 },
+          mappings = {
+            i = {
+              ["<C-x>"] = actions.select_horizontal,
+              ["<C-v>"] = actions.select_vertical,
+            },
+          },
+        }),
+      })
+      telescope.load_extension("fzf")
+    end,
+  },
+
+  -- Git
+  { "tpope/vim-fugitive" },
+  { "tpope/vim-rhubarb" },
+
+  -- Text objects and editing
+  { "wellle/targets.vim" },
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    opts = {},
+  },
+
+  -- Motion (leap = successor to vim-sneak)
+  {
+    url = "https://codeberg.org/andyg/leap.nvim",
+    config = function()
+      map({ "n", "x", "o" }, "s", "<Plug>(leap-forward-to)")
+      map({ "n", "x", "o" }, "S", "<Plug>(leap-backward-to)")
+    end,
+  },
+
+  -- REST client
+  { "diepm/vim-rest-console", ft = "rest" },
+})
+
+-- }}}
+
+-- REST console autocmds {{{
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.rest",
+  callback = function()
+    map("n", "<C-i>", ":call VrcQuery()<CR>", { buffer = true, silent = true })
+  end,
+})
+vim.api.nvim_create_autocmd("BufWinLeave", { pattern = "*.rest", command = "mkview" })
+vim.api.nvim_create_autocmd("BufWinEnter", { pattern = "*.rest", command = "silent! loadview" })
+vim.api.nvim_create_autocmd("BufEnter", { pattern = "__REST.json", command = "set modifiable" })
+
+-- }}}
--- _vimrc
+++ _vimrc
@@ -1,3 +1,5 @@
+" Minimal vimrc — fallback for machines without neovim
+" Full config lives in ~/.config/nvim/init.lua
 
 " General settings {{{
 
@@ -5,53 +7,23 @@ set nocompatible
 let mapleader = ","
 let maplocalleader = ","
 
-" display statusline on every window
 set laststatus=2
-
-" directories for swp files
 set backupdir=~/.vim/backup
 set directory=~/.vim/backup
-
-" use + register for copy/paste
 set clipboard=unnamed
-
-" minimum lines to keep above/below cursor
 set scrolloff=3
-
-" enable persistant undo
-set udf
+set undofile
 set undodir=~/.vim/vimundo
-
-" disable error bell
 set noeb vb t_vb=
-
-" show command keystrokes in down/right corner
 set showcmd
-
-" default file encoding
 set encoding=utf-8
-set fileencoding=utf-8
-
-" activate support for 256-color terminals
 set t_Co=256
-" set Vim-specific sequences for RGB colors
-" see https://github.com/vim/vim/issues/993
 let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
 let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
-
-" don't redraw while executing macros
-set lazyredraw
-
-" enable hidden unsaved buffers
 set hidden
-
-" enable syntax highlighting
-syntax on
 set number
 set ruler
 set cursorline
-
-" indentation and tabs
 set nowrap
 set expandtab
 set tabstop=2
@@ -59,286 +31,73 @@ set shiftwidth=2
 set softtabstop=2
 set autoindent
 set listchars=tab:»\ ,eol:¬,extends:❯,precedes:❮
-noremap <silent><Leader>s :set list!<CR>
-
-" tab completion
 set wildmode=list:longest,list:full
 set wildignore+=*.o,*.obj,.git,*.rbc,*.class,.svn,vendor/gems/*
-
-" allow backspacing over everything in insert mode
 set backspace=indent,eol,start
-
-" configure incremental search
 set hlsearch
 set incsearch
 set ignorecase
 set smartcase
-noremap <silent><Leader><space> :noh<CR>
-
-" split to the right / below current window
 set splitright
 set splitbelow
-
-" highlight line length
 set colorcolumn=120
+set shortmess+=A
+set autoread
+set breakindent
+set viminfo='1024
+set termguicolors
+set background=dark
+set diffopt+=iwhite
 
-" fold vim files
+syntax on
+filetype plugin indent on
 au FileType vim setlocal foldmethod=marker
 
 " }}}
 
-" Adjust defaults {{{
+" Keybindings {{{
 
-" don't bother me when a swap file exists
-set shortmess+=A
-" automatically read changed files
-set autoread
-" adjust indentation of wrapped lines
-set breakindent
-" remap leader
 nnoremap <Leader>; ,
-" don't move cursor on "*"
+noremap <silent><Leader>s :set list!<CR>
+noremap <silent><Leader><space> :noh<CR>
 nnoremap * *<c-o>
-map Oj *
-imap Oj *
-cmap Oj *
-" search for visual selection
 vnoremap * y/<c-r>"<cr><c-o>
-" disable displaying manual when hitting K
 nnoremap K <nop>
-" Reselect visual block after indent/outdent
 vnoremap < <gv
 vnoremap > >gv
-" When pasting, auto-adjust indent and jump to end
 nnoremap p ]p`]
 vnoremap p ]p`]
 vnoremap y y`]
 nnoremap P ]P
-" Navigate to next diff after action
 nmap do do]c
 nmap dp dp]c
-" Make Y behave like other capitals
 map Y y$
-" Improve up/down movement on wrapped lines
 nnoremap j gj
 nnoremap k gk
-" Keep the search matches in the middle of the window
 nnoremap n nzzzv
 nnoremap N Nzzzv
-" Make it easier to navigate links
-nnoremap <c-$> <C-]>
-" Make it easier to exit Terminal mode
 tnoremap <Esc> <C-\><C-n>
-" Increase the amount of remembered old files
-set viminfo='1024
 
-" allows cursor change in tmux mode
-if exists('$TMUX')
-    let &t_SI = "\<Esc>Ptmux;\<Esc>\<Esc>]50;CursorShape=1\x7\<Esc>\\"
-    let &t_EI = "\<Esc>Ptmux;\<Esc>\<Esc>]50;CursorShape=0\x7\<Esc>\\"
-else
-    let &t_SI = "\<Esc>]50;CursorShape=1\x7"
-    let &t_EI = "\<Esc>]50;CursorShape=0\x7"
-endif
-
-" }}}
-
-" Custom mappings {{{
-
-" Emacs style Home/End in insert/command mode
 inoremap <c-a> <esc>I
 inoremap <c-e> <esc>A
 cnoremap <C-a> <Home>
 cnoremap <C-e> <End>
-
-" <Space> toggles folding
 nnoremap <Space> za
 vnoremap <Space> za
-
-" enable quick navigation between folds
 nnoremap zJ zjzmza
 nnoremap zK zkzmza[z
-
-" window shortcuts
 nmap <silent>= :res<CR>:vertical res<CR>
 nmap <Leader>= <C-w>=
 nmap <Leader>l <C-w>L
 nmap <Leader>p <C-w>J
-
-" handier navigation between diffs
 noremap <silent><Leader>d ]czz
 noremap <silent><Leader>D [czz
-
-" close buffer and go up
 noremap <silent><Leader>q :x<CR><C-w>j
-
-" walk through a list of files and show diffs against master
-nmap <silent><Leader>id <c-w>f:resize<CR><c-j><c-j>:Gvdiff master<CR>
-nmap <silent><Leader>if :bd<CR>:bd<CR>j<Leader>id
-nmap <silent><Leader>iF :bd<CR>:bd<CR>dd<Leader>id
-
-" Sudo write
 cmap w!! w !sudo tee % > /dev/null <CR>
-
-" HTML escape the content of Visual selection
 vnoremap <Leader>x :!recode utf8..html<CR>
-
-" format text into tabs
 nmap <silent><Leader>t :%!column -t<CR>
-
-" Diff contents of registers 'a and 'b
 nnoremap <silent><Leader>V :tabnew<CR>:put a<CR>:diffthis<CR>:vnew<CR>:put b<CR>:diffthis<CR>
 nnoremap <silent><Leader>Q :windo bd!<CR>tabclose<CR>
-
-" Toggle 'ignore whitespace' in vimdiff
-set diffopt+=iwhite
-command ToggleIWhite if &diffopt =~ 'iwhite' | set diffopt-=iwhite | else | set diffopt+=iwhite | endif
-nnoremap <silent><Leader>w :ToggleIWhite<CR>
-
-" Run a terminal command silently
-command -nargs=1 Run execute ':call job_start("'.<q-args>.'")'
-nnoremap <Leader>! :Run<Space>
-
-" Replace visual selection with output of command
-vnoremap <Leader>! c<C-R>=system("zsh -lc '" . input(":") . "'")<CR>
-
-" }}} 
-
-" Load plugins / Apply customizations {{{
-
-if empty(glob('~/.vim/autoload/plug.vim'))
-  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
-    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  autocmd VimEnter * PlugInstall --sync
-endif
-
-call plug#begin('~/.vim/plugged')
-
-Plug 'bling/vim-airline'
-Plug 'vim-airline/vim-airline-themes'
-Plug 'arcticicestudio/nord-vim'
-
-Plug 'tpope/vim-fugitive'
-Plug 'tpope/vim-rhubarb'
-
-Plug 'tpope/vim-surround'
-Plug 'tpope/vim-repeat'
-Plug 'wellle/targets.vim'
-Plug 'justinmk/vim-sneak'
-Plug 'tmhedberg/matchit'
-
-Plug 'mileszs/ack.vim'
-Plug 'ervandew/supertab'
-Plug 'junegunn/fzf'
-Plug 'junegunn/fzf.vim'
-Plug 'scrooloose/nerdtree', { 'on':  ['NERDTreeFind', 'NERDTreeToggle'] }
-
-Plug 'diepm/vim-rest-console', { 'for': 'rest' }
-
-Plug 'othree/yajs.vim'
-Plug 'othree/javascript-libraries-syntax.vim'
-Plug 'sheerun/vim-polyglot'
-Plug 'tpope/vim-commentary'
-
-Plug 'tmux-plugins/vim-tmux-focus-events'
-" Plug 'nvim-lua/plenary.nvim'
-" Plug 'greggh/claude-code.nvim'
-
-" Add plugins to &runtimepath
-call plug#end()
-
-" lua require('claude-code').setup()
-
-" shortcut for PlugStatus
-nnoremap <silent><Leader>S :PlugStatus<CR>
-
-" default colorscheme
-set termguicolors
-set background=dark
-" colorscheme solarized8
-colorscheme nord
-
-" set airline plugin to use fancy symbols
-let g:airline_powerline_fonts = 1
-if !exists('g:airline_symbols')
-  let g:airline_symbols = {}
-endif
-let g:airline_symbols.dirty = " \u2042"
-
-" force sneak into label mode
-let g:sneak#use_ic_scs = 1
-nmap s <Plug>SneakLabel_s
-nmap S <Plug>SneakLabel_S
-nmap g; <Plug>Sneak_,
-
-" Customize targets
-let g:targets_pairs = '()b {}c []B'
-let g:targets_argOpening = '[({[]'
-let g:targets_argClosing = '[]})]'
-
-" set supertab completion scheme
-set completeopt=longest,menuone,preview
-let g:SuperTabDefaultCompletionType = "<c-n>"
-let g:SuperTabLongestHighlight = 1
-let g:SuperTabLongestEnhanced = 1
-
-let g:fugitive_gitlab_domains = ['https://gitlab.adam']
-let g:github_enterprise_urls = ['https://ghe.spotify.net']
-
-" configure NERDTree toggler
-let g:NERDTreeWinSize=60
-let g:NERDTreeMinimalUI=1
-let g:NERDTreeQuitOnOpen=1
-let g:NERDTreeAutoDeleteBuffer=1
-let g:NERDTreeDirArrowExpandable = '+'
-let g:NERDTreeDirArrowCollapsible = '-'
-nnoremap <silent><Leader>nd :NERDTreeToggle<CR>
-nnoremap <silent><Leader>nf :NERDTreeFind<CR>
-
-" configure FZF
-noremap <silent><Leader>nt :call fzf#run(fzf#wrap({'source': 'ag --hidden --ignore .git -g ""'}))<CR>
-noremap <silent><Leader>nb :Buffers<CR>
-noremap <silent><Leader>ne :History<CR>
-noremap <silent><Leader>nr :History:<CR>
-noremap <silent><Leader>nc :BCommits!<CR>
-let g:fzf_layout = { 'down': '~30%' }
-let g:fzf_action = {
-  \ 'ctrl-x': 'split',
-  \ 'ctrl-v': 'vsplit',
-  \ 'ctrl-d': 'vertical diffsplit' }
-
-" Ag
-let g:ackprg = 'ag --vimgrep'
-" searches for the word under the cursor
-nnoremap <silent><Leader>G :Ack! "\b<cword>\b"<CR>
-
-" REST Console
-let g:vrc_set_default_mapping = 0
-let g:vrc_allow_get_request_body = 1
-let g:vrc_elasticsearch_support = 1
-let b:vrc_response_default_content_type = 'application/json'
-let g:vrc_output_buffer_name = '__REST.json'
-let g:vrc_curl_opts = {
-  \ '-b': $HOME . '/.vim/backup/vrc_cookie_jar',
-  \ '-c': $HOME . '/.vim/backup/vrc_cookie_jar',
-  \ '-s': '',
-  \ '-L': '',
-  \ '-k': '',
-\}
-au BufNewFile,BufRead *.rest nmap <silent><c-i> :call VrcQuery()<CR>
-au BufWinLeave *.rest mkview 
-au BufWinEnter *.rest silent! loadview
-
-au BufEnter __REST.json set modifiable
-
-" Remap vim-commentary
 map ,c gc
-
-" Enable quotes concealing for JSON files
-let g:vim_json_syntax_conceal = 1
-
-" Otherwise, YaJS would assign ft=javascript.jsx, and that
-" has some slight differences in syntax highliting
-au BufNewFile,BufRead *.js set ft=javascript
 
 " }}}
