e4bd7492375134c77d4e9d513bde94419bb203ad
tonyskn <tonyskn@gmail.com>
2026-07-15 09:56:28 +0200

    cl

--- _claude/hooks/clear-attention.sh
+++ _claude/hooks/clear-attention.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 WINDOW_ID=$(tmux display-message -t "$TMUX_PANE" -p '#{window_id}' 2>/dev/null)
-PANE_TITLE=$(tmux display-message -t "$TMUX_PANE" -p '#W' 2>/dev/null)
-# Strip any prefix
-CLEAN_TITLE="${PANE_TITLE#? }"
-CLEAN_TITLE="${CLEAN_TITLE#- }"
-tmux rename-window -t "$WINDOW_ID" "${CLEAN_TITLE}"
+TITLE=$(tmux display-message -t "$TMUX_PANE" -p '#W' 2>/dev/null)
+TITLE="${TITLE#? }"
+tmux rename-window -t "$WINDOW_ID" "● ${TITLE}"
--- _claude/hooks/reset-marker.sh
+++ _claude/hooks/reset-marker.sh
@@ -2,4 +2,4 @@
 WINDOW_ID=$(tmux display-message -t "$TMUX_PANE" -p '#{window_id}' 2>/dev/null)
 TITLE=$(tmux display-message -t "$TMUX_PANE" -p '#W' 2>/dev/null)
 TITLE="${TITLE#? }"
-tmux rename-window -t "$WINDOW_ID" -- "○ ${TITLE}"
+tmux rename-window -t "$WINDOW_ID" "${TITLE}"
