b7325926585e9198ff2e6a9cb7f756a0d4fc17d3
tonyskn <tonyskn@gmail.com>
2026-07-16 08:16:48 +0200

    tmux-marker

--- _claude/hooks/reset-marker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-WINDOW_ID=$(tmux display-message -t "$TMUX_PANE" -p '#{window_id}' 2>/dev/null)
-TITLE=$(tmux display-message -t "$TMUX_PANE" -p '#W' 2>/dev/null)
-TITLE="${TITLE#? }"
-tmux rename-window -t "$WINDOW_ID" "${TITLE}"
